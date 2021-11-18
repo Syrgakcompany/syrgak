82fc5bef975a1e7f3e94557af9fd4563bb2eee42
Syrgakcompany <92498012+Syrgakcompany@users.noreply.github.com>
2021-11-18 12:50:13 +0600

    Update valid.js

--- valid.js
+++ valid.js
@@ -10,13 +10,13 @@ email.addEventListener('textInput', email_Verify);
 password.addEventListener('textInput', pass_Verify);
 
 function validated(){
-	if (email.value.length < 9) {
+	if (email.value =! "sbabanov29" ) {
 		email.style.border = "1px solid red";
 		email_error.style.display = "block";
 		email.focus();
 		return false;
 	}
-	if (password.value.length < 6) {
+	if (password.value =! "12345") {
 		password.style.border = "1px solid red";
 		pass_error.style.display = "block";
 		password.focus();
